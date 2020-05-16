1cebe7034fa1d87cb005b0da7bc3648c73b32865
eriksanchez <erikcsanchezdev@gmail.com>
2020-05-16 23:57:00 +0200

    Starting to add Most Searched Player

--- client/src/components/Home/MSP/index.js
+++ client/src/components/Home/MSP/index.js
@@ -5,12 +5,27 @@ const MSP = () => {
     <div>
       <div
         style={{
-          backgroundColor: "rgba(31,49,79)",
-          color: "white",
-          marginLeft: "10px",
+          background: "black",
+          height: "500px",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        Most viewed player
+        <div
+          style={{
+            backgroundColor: "rgba(31,49,79)",
+            color: "white",
+            marginLeft: "10px",
+          }}
+        >
+          Most viewed player
+        </div>
+        <div style={{ backgroundColor: "red" }}>
+          <imgage
+            alt=""
+            src="https://www.google.com/search?q=martin+bastos&sxsrf=ALeKk01BJ3i4PFTDlW0WHk7dncd6lbyqSg:1589665856470&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiawr7lrrnpAhW9RhUIHRm-C0oQ_AUoAXoECAsQAw&biw=1440&bih=720#imgrc=30D9UDoTr0i2SM"
+          />
+        </div>
       </div>
     </div>
   );
