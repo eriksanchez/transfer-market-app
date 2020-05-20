65362636c29964ff4eaaca3f95e70f21a7f26af0
eriksanchez <erikcsanchezdev@gmail.com>
2020-05-20 19:51:56 +0200

    MSP additions...

--- client/src/components/Home/MSP/index.js
+++ client/src/components/Home/MSP/index.js
@@ -28,34 +28,42 @@ const MSP = () => {
               src="https://www.toscanagol.it/immagini/TO_immagini/foto/big/019064castelnuovo.jpg"
             />
           </div>
+
           <div>
             <div style={{ display: "flex", flexDirection: "row" }}>
-              <h2 style={{ color: "rgba(59,118,160)" }}>Martin Bastos</h2>
+              <h5 style={{ color: "rgba(59,118,160)" }}>Martin Bastos</h5>
               <img
-                style={{ height: "30px", width: "30px", margin: "5px" }}
+                style={{ height: "25px", width: "25px", margin: "5px" }}
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1280px-Flag_of_Uruguay.svg.png"
               />
             </div>
-            <div
-              style={{ display: "flex", flexDirection: "column", color: "red" }}
-            >
-              <h5>
-                Market Value: <h6>$250,000</h6>
-              </h5>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              Market Value:
+              <h6 style={{ marginLeft: "5px", marginTop: "2px" }}>$250,000</h6>
             </div>
+
+            <div> #1</div>
+
             <div
               style={{
                 display: "flex",
                 flexDirection: "row",
+                backgroundColor: "rgba(242,242,242)",
+                alignItems: "center",
               }}
             >
               <img
-                style={{ height: "50px", width: "50px", margin: "5px" }}
+                style={{ height: "50px", width: "50px", margin: "7px" }}
                 src="https://www.tuttocampo.it/Web/Images/Teams/Original/921145.png?v9"
                 alt=""
               />
-              <h6 style={{ wordWrap: "break-word" }}>
-                Club: U.S. Castelnuovo Garfagnana
+              <h6
+                style={{
+                  wordWrap: "break-word",
+                  margin: "5px",
+                }}
+              >
+                U.S. Castelnuovo Garfagnana
               </h6>
             </div>
           </div>
