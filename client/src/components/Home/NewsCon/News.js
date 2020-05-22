f69b061c49f53d2b9e2efcf9915a8ab8e96c9c66
eriksanchez <erikcsanchezdev@gmail.com>
2020-05-22 23:53:13 +0200

    News Component...

--- client/src/components/Home/NewsCon/News.js
+++ client/src/components/Home/NewsCon/News.js
@@ -2,9 +2,27 @@ import React from "react";
 
 const News = (props) => {
   return (
-    <div style={{ marginTop: "10px" }}>
-      <h4>ALL NEWS OF MAY 20, 2020</h4>
-      This is a News component
+    <div
+      style={{
+        marginTop: "10px",
+        backgroundColor: "red",
+        height: "50px",
+        width: "50%",
+        marginLeft: "10px",
+      }}
+    >
+      <div style={{ backgroundColor: "rgba(228,228,228)" }}>
+        22.05.2020 - 22:03
+      </div>
+      <div>
+        <img
+          alt=""
+          src="https://tmssl.akamaized.net/images/portrait/medium/517894-1532351797.jpg?lm=1532351893"
+        />
+        <h6 style={{ color: "rgba(74,171,231)" }}>
+          First derby at Olympiastadion
+        </h6>
+      </div>
     </div>
   );
 };
