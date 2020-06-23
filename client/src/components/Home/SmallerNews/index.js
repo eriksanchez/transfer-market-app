49b7db6b2521d7fc8a007da4088144f3581315cb
eriksanchez <erikcsanchezdev@gmail.com>
2020-06-23 15:37:44 +0200

    Added to News section...

--- client/src/components/Home/SmallerNews/index.js
+++ client/src/components/Home/SmallerNews/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const News = (props) => {
+const SmallerNews = (props) => {
   return (
     <div
       style={{
         marginTop: "10px",
 
-        width: "60%",
+        width: "30%",
         marginLeft: "10px",
         border: "2px solid",
       }}
@@ -20,14 +20,18 @@ const News = (props) => {
         <h6
           style={{
             display: "inline-block",
-            marginLeft: "15%",
+            marginLeft: "28%",
           }}
         >
           24.05.2020 - 20:02
         </h6>
         <img
           alt=""
-          style={{ position: "absolute", left: "20px", marginTop: "15px" }}
+          style={{
+            position: "absolute",
+            left: "20px",
+            marginTop: "15px",
+          }}
           src="https://tmssl.akamaized.net/images/portrait/medium/517894-1532351797.jpg?lm=1532351893"
         />
       </div>
@@ -43,20 +47,20 @@ const News = (props) => {
           <h6
             style={{
               display: "inline-block",
-              marginLeft: "15%",
+              marginLeft: "28%",
               color: "rgba(74,171,231)",
             }}
           >
             First derby at Olympiastadion
           </h6>
-          <h3 style={{ display: "inline-block", marginLeft: "15%" }}>
+          <h6 style={{ display: "inline-block", marginLeft: "28%" }}>
             Kanya Fujimoto nears Portugal move - 20-year-old became regular this
             season
-          </h3>
+          </h6>
           <p
             style={{
               display: "inline-block",
-              marginLeft: "15%",
+              marginLeft: "28%",
               maxHeight: "50px",
               position: "relative",
               overflow: "hidden",
@@ -73,4 +77,4 @@ const News = (props) => {
   );
 };
 
-export default News;
+export default SmallerNews;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import NewsSlider from "./NewsSlider";
 import MSP from "./MSP";
 import NewsCon from "./NewsCon/index";
+import SmallerNews from "./SmallerNews";
 
 const Home = () => {
   return (
@@ -14,6 +15,10 @@ const Home = () => {
       <div style={{ backgroundColor: "rgba(255,255,255)" }}>
         <NewsCon />
         <NewsCon />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <SmallerNews />
+          <SmallerNews />
+        </div>
       </div>
     </div>
   );
